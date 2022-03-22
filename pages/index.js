190fe5cd22d47cd3da176c52ffd90ae8c74e99ed
yutthapichai chaiwong <yutthapichai.c@skanhubrewards.com>
2022-03-22 14:01:53 +0700

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
 import Default from "../components/layouts/Default";
 
 export default function Home() {
